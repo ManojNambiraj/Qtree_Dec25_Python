2858fb271094656c2ecb23852bbdf60c1de40f4a
ManojNambiraj <postboxmano@gmail.com>
2026-01-02 20:30:36 +0530

    Functions

---
+++ Class7/functions.py
@@ -0,0 +1,52 @@
+# Functions:  --> DRY
+
+    # def Multi(a, b):           # Function Definition
+    #     return a * b
+
+    # print(Multi(100, 2))
+
+    # def Add(a, b, c):           
+    #     return a + b + c
+
+    # print(Add(100, 2, 3))
+
+    # def sub():
+    #     pass
+
+    # sub()
+
+# Lambda Function:
+
+    # demo = lambda a, b : a * b
+
+    # print(demo(5, 10))
+
+# Types of Arguments:
+
+    # Positional Arguments
+
+        # def demo(a, b, c):
+        #     print(a, b, c)
+
+        # demo(2, 7, 9)
+
+    # Default Arguments
+
+        # def demo(a, b, c = 1):
+        #     print(a, b, c)
+
+        # demo(2, 7, 9)
+
+    # Keyword Arguments
+
+        # def demo(a, b, c):
+        #     print(a, b, c)
+
+        # demo(c = 2, a = 7, b = 9)
+
+    # Arbitary Arguments
+
+        # def demo(a, *b):
+        #     print(a, b)
+
+        # demo(2, 7, 9, 10, 11)
